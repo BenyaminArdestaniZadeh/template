91912ed7f82b519aa7953e623ea7afd1de16db00
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2023-12-07 21:13:33 +0330

    feat: add theme to layout root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,18 @@
+import { Theme } from "@radix-ui/themes";
+import type { Metadata } from "next";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import "./theme.config.css";
+//create theme.config.css file in app directory and add custom radix css inside that
+
+//create font config file for custom font
+
+//create metadata for app
+
+// export const metadata: Metadata = {
+//   title: "رده‌بندی",
+//   description: "پنل مدیریت رده‌بندی",
+// };
 
 export default function RootLayout({
   children,
@@ -7,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Theme scaling={"100%"} radius={"large"}>
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
